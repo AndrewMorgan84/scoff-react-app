32a0ae3d92abcf9cc44981535a13a2bb2d8009dc
Andrew <andrew.morgan@e.org>
2022-02-10 13:31:11 +0000

    Added loading and error handing functionality

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,10 +6,16 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
     useEffect(() => {
       const fetchMeals = async () => {
+        setIsLoading(true);
         const response = await fetch('https://scoff-react-app-default-rtdb.firebaseio.com/meals.json');
+        if(!response.ok){
+          throw new Error('Something went wrong!');
+        };
         const responseData = await response.json();
         const loadedMeals = [];
         for(const key in responseData){
@@ -21,9 +27,27 @@ const AvailableMeals = () => {
           });
         }
         setMeals(loadedMeals);
+        setIsLoading(false);
       };
-      fetchMeals();
+
+      fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      }); 
+
     }, []);
+
+    if(isLoading){
+      return <section className={styles.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    }
+
+    if(httpError){
+      return <section className={styles.mealsError}>
+        <p>{httpError}</p>
+      </section>
+    }
 
     const mealsList = meals.map((meal) => (<MealItem
           id={meal.id}
